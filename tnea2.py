fd82e72ad33e87d423e59ad8ae785b850ddbcb2e
BAHURDEEN U <ubahurdeen@gmail.com>
2025-07-24 11:23:45 +0530

    Update tnea2.py

--- tnea2.py
+++ tnea2.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or restrict to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/3011")
 def nit_trichy():
